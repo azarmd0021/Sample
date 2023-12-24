9d5c55896b747ae30f1ef9cb166cca31022bb039
azarmd0021 <azarmd0021@gmail.com>
2023-12-24 15:50:57 +0530

    pls update

--- src/test/java/com/app/base/BaseClassDummy.java
+++ src/test/java/com/app/base/BaseClassDummy.java
@@ -4,9 +4,7 @@ public class BaseClassDummy {
 	public static void main(String[] args) {
 		System.out.println("Azar");
 		System.out.println("Azar");
-		System.out.println("Azar1");
-		System.out.println("Azar1");
-		System.out.println("Azar1");
+		
 	}
 
 }
