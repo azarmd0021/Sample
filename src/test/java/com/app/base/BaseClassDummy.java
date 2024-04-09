41c8d449e496520a0d1dd3bbdfb7470c20a7bdde
azarmd0021 <153576922+azarmd0021@users.noreply.github.com>
2024-04-09 20:28:33 +0530

    Update BaseClassDummy.java

--- src/test/java/com/app/base/BaseClassDummy.java
+++ src/test/java/com/app/base/BaseClassDummy.java
@@ -4,6 +4,7 @@ public class BaseClassDummy {
 	public static void main(String[] args) {
 		System.out.println("Azar");
 		System.out.println("Azar");
+		System.out.println("ameera");
 		System.out.println("bilala");
 		System.out.println("amsar");
 		System.out.println("papu Janntha");
